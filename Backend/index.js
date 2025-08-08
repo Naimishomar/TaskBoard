68067dc8b07b1fcb5362aeaa22c121f843dd506d
Naimish Omar <naimishomar@gmail.com>
2025-08-08 11:02:42 +0530

    cors ploicy update

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 const allowedOrigins = [
-  'http://localhost:3000',
+  'http://localhost:3001',
   process.env.FRONTEND_URL,
 ];
 
@@ -26,12 +26,16 @@ app.use(cors({
   },
   credentials: true,
 }));
-
+app.options('*', cors());
 app.use(express.json());
 
 app.use('/api/user', authRoutes);
 app.use('/api/boards', boardRoutes);
 app.use('/api/tasks', taskRoutes);
+
+app.get('/cors-test', (req, res) => {
+  res.json({ message: 'CORS working!' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
