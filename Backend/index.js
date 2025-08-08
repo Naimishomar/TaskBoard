eede9ddfd8f9db2b8f4606e4e063a4876bc53ef9
Naimish Omar <naimishomar@gmail.com>
2025-08-08 10:33:35 +0530

    Update cors policy

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,10 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/user', authRoutes);
